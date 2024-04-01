a83b222283fc4c900d61687e98db19553849bd04
TravisJBlack <159502505+TravisJBlack@users.noreply.github.com>
2024-04-01 18:49:28 -0500

    add 3 functions to handle getting user input, city id, and then getting the hotels

--- Planner.js
+++ Planner.js
@@ -1,4 +1,6 @@
 const requestURL = '';
+const cityInputEL = document.getElementById('city');
+const submitButton = document.getElementById('city-button');
 
 fetch(requestURL)
 
@@ -15,3 +17,56 @@ function currency_api(base,exchange) {
     });
 }
 currency_api("USD","EUR,CAD")
+
+const citySubmit = function (){
+  const searchedCity = cityInputEL.value.trim();
+
+  if(searchedCity) {
+    getLocation(searchedCity);
+  }
+;}
+
+const getLocation = function (city){
+  const url = `https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations?name=${city}&search_type=HOTEL`;
+  const options = {
+	  method: 'GET',
+	  headers: {
+		  'X-RapidAPI-Key': 'ff13b66632mshee3571a2fc826dfp1bdf44jsn46e68ce70f5b',
+		  'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+	  }
+  };
+
+  fetch(url, options).then(function (response) {
+    if(response.ok) {
+      response.json().then(function (data) {
+        const cityId = 
+        console.log(data);
+      })
+    } else {
+      console.log(error);
+    }
+  });
+};
+
+const hotels = function (){
+  const hotelUrl = 'https://priceline-com-provider.p.rapidapi.com/v1/hotels/search?location_id=3000012264&date_checkin=2024-07-24&date_checkout=2024-07-25&sort_order=PROXIMITY&rooms_number=1';
+  const hotelOptions = {
+	  method: 'GET',
+	  headers: {
+		  'X-RapidAPI-Key': 'ff13b66632mshee3571a2fc826dfp1bdf44jsn46e68ce70f5b',
+		  'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+	  }
+  };
+
+  fetch(hotelUrl, hotelOptions).then(function (response) {
+    if(response.ok) {
+      response.json().then(function (data) {
+      console.log(data);
+      })
+    } else {
+    console.log(error);
+   }
+  });
+};
+
+submitButton.addEventListener('click', citySubmit);
