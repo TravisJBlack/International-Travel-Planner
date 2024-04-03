2636eef95aa838cc56bbd494a437b74b822d4dfa
Ash53rd <aandersonatx@gmail.com>
2024-04-03 15:58:47 -0500

    fixed merge

--- Planner.js
+++ Planner.js
@@ -3,6 +3,25 @@ const cityInputEL = document.getElementById('city');
 const checkInEl = document.getElementById('check-in');
 const checkOutEl = document.getElementById('check-out');
 const submitButton = document.getElementById('itinerary');
+const requestURL = '';
+
+
+
+const apiKey = `fca_live_nfY0OLbxe8dNoSBSSA33dKeIJhTJhWVgG5u2SSW1`
+function currency_api(base,exchange) {
+  const queryurl = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}&base_currency=${base}&currencies=${exchange}`;
+  fetch(queryurl)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+currency_api("USD","EUR,USD,JPY,BGN,CZK,DKK,GBP,HUF,PLN,RON,SEK,CHF,ISK,NOK,HRK,RUB,TRY,AUD,BRL,CAD,CNY,HKD,IDR,ILS,INR,KRW,MXN,MYR,NZD,PHP,SGD,THB,ZAR")
+
+
 
 //fucntion to handle the input from the user and sending the info to another function
 const citySubmit = function (){
